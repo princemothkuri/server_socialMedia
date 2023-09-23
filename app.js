e56a01b40ac6044e0684982241869507f30f3371
princemothkuri <princemothkuri@gmail.com>
2023-09-23 21:30:47 +0530

    cors

--- app.js
+++ app.js
@@ -3,6 +3,15 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const app = express();
+var cors = require("cors");
+app.use(cors());
+
+var corsOptions = {
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 // app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(bodyParser.json());
